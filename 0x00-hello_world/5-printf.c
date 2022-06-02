7c388edbf800f521c8f3534604e5778b2200ec02
Kolo <konatealim@gmail.com>
2022-06-02 15:46:32 -0700

    io

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
  *
- * main - Entry
+ * main - Entry point
  *
  * Return : Always 0 (Success)
  *
@@ -10,4 +11,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}		
+}
