1a80da7f1b76dd6505a61b3ddc96ab17a05ee4b6
Kolo <konatealim@gmail.com>
2022-06-02 16:36:59 -0700

    kjr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 1 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+		write(1, "and that piece of art is useful\n", 59);
+
+			return (1);
+
+}
