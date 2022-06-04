fe162670497f9846ebaeef29372efbef2cb6be61
Kolo <konatealim@gmail.com>
2022-06-04 11:18:14 -0700

    k

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,14 @@ int main(void)
 	int ch;
 
 	for (ch = 0; ch <= 9; ch++)
+	{
 		putchar(ch + '0');
 		if (ch < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
