41b6cbd18a4425a3eb60f9c3a51fc9f29cf1d537
Kolo <konatealim@gmail.com>
2022-06-04 12:31:01 -0700

    r

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,22 +13,22 @@ int main(void)
 	int chi;
 	int nom;
 
-	for (ch = 0; ch <= 9; ch ++)
+	for (ch = 0; ch <= 9; ch++)
 	{
 		for (num = 0; num <= 9; num++)
 		{
 			for (chi = 0; chi <= 9; chi++)
 			{
-				for (nom = 0; nom <= 9;nom ++)
+				for (nom = 0; nom <= 9; nom++)
 				{
 					if (ch * 10 + num < chi * 10 + nom)
 					{
-						putchar(ch + '0');                                  
+						putchar(ch + '0');
 						putchar(num + '0');
 						putchar(' ');
 						putchar(chi + '0');
 						putchar(nom + '0');
-						if (ch * 1000 + num *100 + chi * 10 + nom != 9899)
+						if (ch * 1000 + num * 100 + chi * 10 + nom != 9899)
 						{
 							putchar(',');
 							putchar(' ');
