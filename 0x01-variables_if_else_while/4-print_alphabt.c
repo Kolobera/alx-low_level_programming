234dd5c7f07e87db632756c459e2f1f65b224356
Kolo <konatealim@gmail.com>
2022-06-04 10:29:24 -0700

    p

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,11 @@
 int main(void)
 {
 	int ch;
-	for (ch='a'; ch <= 'z'  && ch != 'q' && ch != 'e'; ch++)
+	for (ch='a'; ch <= 'z'; ch++)
+	{
+		if ( ch != 'q' && ch != 'e')
 		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
