3000e7a4bb53d0f1547b3ef9081b6c9e67ca5bbe
Kolo <konatealim@gmail.com>
2022-06-04 10:08:26 -0700

    p

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,12 @@
 int main(void)
 {
 	int ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar (ch);
+		for (ch = 'a'; ch <= 'z'; ch++)
+			putchar (ch);
 		putchar ('\n');
-	return (0);
+	
+		
+		return (0);
 
 }
 
