d4e2865815ba334a3b22011f195016462fc9a654
Kolo <konatealim@gmail.com>
2022-06-04 10:58:30 -0700

    k

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch-=1)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
