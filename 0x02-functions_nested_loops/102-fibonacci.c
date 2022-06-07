090427d7ccf334602240312c1ae833c19ce44fd4
Kolo <konatealim@gmail.com>
2022-06-07 16:53:36 -0700

    kf

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	int c;
+	int s;
+
+	for (c = 1; c <= 50; c++)
+	{
+		if (c <= 49)
+		{
+		s = a + b;
+		printf("%d, ");
+		a = b; 
+		b = s;
+		}
+		else
+			printf("%d\n", a + b);
+	}
+	return (0);
+
