c1bef588d4ca44d192a16f3c404e79c2a4575396
Kolo <konatealim@gmail.com>
2022-06-07 15:35:48 -0700

    kf

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -13,31 +13,6 @@
 
 int add(int n, int c)
 {
-	if (n + c < 0)
-	{
-		if (n + c > -10)
-		{
-			_putchar(45);
-			_putchar(-(n + c) + 48);
-		}
-		else
-		{
-			_putchar(45);
-			_putchar(((-(n + c) + (-(n + c) % 10)) / 10) + 48);
-			_putchar((-(n + c)) % 10 + 48);
-		}
-	}
-	else
-	{
-		if (n + c < 10)
-			_putchar( (n + c) + 48);
-		else
-		{
-			_putchar(((n + c + (n + c) % 10) / 10) + 48);
-			_putchar((n + c) % 10 + 48);
-		}
-	}
-	_putchar('\n');
 	return (n + c);
 }
 
