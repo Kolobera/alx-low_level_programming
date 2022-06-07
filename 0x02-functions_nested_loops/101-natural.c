6f4661767ebfedfd8b243eb4b1a6299ace64ec1f
Kolo <konatealim@gmail.com>
2022-06-07 16:33:15 -0700

    kf

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * counti - count
+ *
+ * Return: 0
+ */
+
+void counti(void)
+{
+	int a;
+	int s = 0:
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			s=s+a
+		}
+	}
+	printf("%d\n", s);
+}
