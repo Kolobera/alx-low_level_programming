a57caa97034fa8ec246af54f059446df37641b82
Kolo <konatealim@gmail.com>
2022-06-07 13:19:18 -0700

    kf

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Calculate the valeur absolue
+ *
+ *@n: integer
+ *
+ * Return: The absolute val
+ */
+
+int _abs(int n);
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
