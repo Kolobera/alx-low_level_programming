9839764869cab2df37d8edaf488ef79258a02dc3
Kolo <konatealim@gmail.com>
2022-06-07 06:06:20 -0700

    kf

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_is_lower - Tell if lowercase
+ *
+ *Return: 0 or 1
+ */
+
+int _is_lower(char c)
+{
+	bool re;  
+		
+	re = is_lower(int c);
+	
+	return (re);
+}
