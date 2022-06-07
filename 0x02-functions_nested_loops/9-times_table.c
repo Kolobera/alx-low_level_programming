5de33239d8884a29439adb425501518d6b8d0103
Kolo <konatealim@gmail.com>
2022-06-07 14:39:20 -0700

    kf

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,34 +9,27 @@
 
 void times_table(void)
 {
-	int a, b, c, d;
+	int a, b, c;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
-			if (a * b < 10)
-				_putchar(a * b + 48);
-			
-			else
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
 			{
-				c = (a * b - (a * b) % 10 ) / 10;
-				d = (a * b) % 10;
+				_putchar(32);	
 				_putchar(c + 48);
-				_putchar(d + 48);
-			}
-
-			if (b != 9)
-			{
-				_putchar(44);
-				_putchar(32);
-				if (a * b < 10)
-					_putchar(32);
 			}
 			else
-				_putchar(36);
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}
 }
-
