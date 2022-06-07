2d981eb0cb2d9762f2a582ec7e1b9dc573264311
Kolo <konatealim@gmail.com>
2022-06-07 14:01:06 -0700

    kf

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,41 +15,22 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 48; a <= 50; a+;+)
 	{
-		if (a <= 1)
+		for (b = 48; b <= 57; b++)
 		{
-			for (b = 0; b <= 9; b++)
+			for (c = 48;c <= 53; c++)
 			{
-				for (c = 0;c <= 5; c++)
+				for (d = 48; d <= 57; d++)
 				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a);
-						_putchar(b);
-						_putchar(";");
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
-					}
-				}
-			}
-		}
-		else
-		{
-			for (b = 0; b <= 3; b++)
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a);
-						_putchar(b);
-						_putchar(";");
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
-					}
+					if (a >= 50 && b >= 52)
+						break;
+					_putchar(a);
+					_putchar(b);	
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
 				}
 			}
 		}
