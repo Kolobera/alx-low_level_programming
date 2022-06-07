2f693ebf71550c0018c3a9d093c0e5cb4462331a
Kolo <konatealim@gmail.com>
2022-06-07 12:58:09 -0700

    kf

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Tell whether alpha
+ *
+ * @c: c la fête
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) or (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
