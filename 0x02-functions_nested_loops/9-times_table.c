4f45adaeba3e6a209a878c13dfee908fc407127e
Kolo <konatealim@gmail.com>
2022-06-07 14:26:25 -0700

    kf

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,22 @@
 
 void times_table(void)
 {
-	int a, b;
+	int a, b, c, d;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(a*b + 48);
+			if (a*b < 10)
+				_putchar(a * b + 48);
+			else
+			{
+				c = (a * b - (a * b) % 10 ) / 10;
+				d = (a * b) % 10;
+				_putchar(c + 48);
+				_putchar(d + 48);
+			}
+
 			if (b != 9)
 			{
 				_putchar(44);
