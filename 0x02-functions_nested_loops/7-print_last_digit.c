a5b6559443757070e5ab537881c2581a8b475cd0
Kolo <konatealim@gmail.com>
2022-06-07 13:26:27 -0700

    kf

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - 	Entry
+ *
+ *@n: Number
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int la;
+
+	la = n % 10;
+	retrun (la);
+}
