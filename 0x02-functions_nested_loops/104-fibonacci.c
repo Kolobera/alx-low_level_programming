3a64308687f4516faa0c7280b065f67e002d5865
Kolo <konatealim@gmail.com>
2022-06-07 17:47:15 -0700

    kf

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,13 +6,13 @@
  *
  *  * main - main block
  *
- *   * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ *  * Description: Find and print the first 98 fib numbers starting with 1 and 2.
  *
- *    * Numbers should be coma and space separated.
+ *  * Numbers should be coma and space separated.
  *
- *     * Return: 0
+ *  * Return: 0
  *
- *      */
+ */
 
 int main(void)
 {
