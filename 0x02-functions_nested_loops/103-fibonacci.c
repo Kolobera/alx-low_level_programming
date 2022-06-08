fb650504e96fd578349e83dfefb1d2b40ae3d7e3
Kolo <konatealim@gmail.com>
2022-06-07 17:21:05 -0700

    kf

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - fibo even
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long a = 0;
+	long b = 1;
+	int c;
+	long s;
+	long d;
+
+	while (s < 4000000)
+	{
+		if (b % 2 == 1)
+			d += b;
+		s = a + b;
+		a = b;
+		b = s;
+	}
+	printf("%ld\n", d);
+	return (0);
+}
