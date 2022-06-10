8649aa903058fcd2e4d45720f60d9044ffc9f4cb
Kolo <konatealim@gmail.com>
2022-06-10 16:47:46 -0700

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * print_numbers - print digits
+ *
+ * Return: none
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	       _putchar(ch);
+	putchar('\n');
+}
