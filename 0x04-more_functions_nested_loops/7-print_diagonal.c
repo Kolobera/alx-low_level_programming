73e2296d457ef442ba55fc7408514a3be965fdb8
Kolo <konatealim@gmail.com>
2022-06-10 18:20:30 -0700

    add

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,12 @@ void print_diagonal(int n)
 	int ch;
 	int i;
 
-	for (ch = 1; ch <= n; ch++)
+	for (ch = 0; ch < n; ch++)
 	{
-		for (i = 1; i <= ch-1; i++)
+		for (i = 0; i <= ch; i++)
 			_putchar(32);
 		_putchar(92);
 		_putchar('\n');
 	}
 	_putchar('\n');
 }
-
