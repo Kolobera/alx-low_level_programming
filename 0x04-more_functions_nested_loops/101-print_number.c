70ef65885a8b06a28bcff06bda6210f35a3dea52
Kolo <konatealim@gmail.com>
2022-06-10 19:51:02 -0700

    add

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,20 +18,20 @@ void print_number(int n)
 
 	if (n < 10 && n >= 0)
 	{
-		_putchar (n + '0');
+		_putchar(n + '0');
 	}
 	else if (n > -10 && n < 0)
 	{
 		n = n - 2 * n;
 		_putchar('-');
-		_putchar (n + '0');
+		_putchar(n + '0');
 	}
 	else
 	{
 		if (n < 0)
 		{
 			n = n * -1;
-			_putchar ('-');
+			_putchar('-');
 		}
 		z = n;
 	while (z / b > 9)
@@ -42,7 +42,7 @@ void print_number(int n)
 	{
 		m = z / b;
 		z = z % b;
-		_putchar (m + '0');
+		_putchar(m + '0');
 		b = b / 10;
 	}
 	}
