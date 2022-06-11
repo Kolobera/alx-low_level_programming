3dc42fee11d4649ab15bf89f65a0009b79c56587
Kolo <konatealim@gmail.com>
2022-06-10 19:00:33 -0700

    add

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,17 @@ int main(void)
 
 	for (ch = 1; ch <= 100; ch++)
 	{
-		if (ch % 3 == 0)
+		if ((ch % 3) == 0 && (ch % 5) != 0)
 			printf("Fizz ");
-		else if (ch % 5 == 0)
+		else if ((ch % )5 == 0 && (ch % 3) != 0)
 			printf("Buzz ");
+		else if (ch == 100)
+			printf("Buzz");
+		else if ((ch % 3 == )0 && (ch % )5 == 0)
+			printf("FizzBuzz ")
 		else
-			printf("%d ", ch);
+			printf("%d ", ch)
 	}
+	printf('\n')
 	return (0);
 }
