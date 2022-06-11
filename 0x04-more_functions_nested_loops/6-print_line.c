6a96f021202863b39032c267c6c31ea45740af13
Kolo <konatealim@gmail.com>
2022-06-10 17:46:09 -0700

    add

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print
+ *@n: number to print
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int ch;
+
+	for (ch = 0; ch < n; ch++)
+		_putchar(95);
+}
+
