b4e2ed2d02a8bbcaa5ac58a9564fca5999610615
Kolo <konatealim@gmail.com>
2022-06-10 18:03:40 -0700

    add

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonale
+ * @n: int
+ * Return: None
+ */
+
+void print_diagonal(int n)
+{
+	int ch;
+	int i;
+	
+	for (ch = 1; ch <= n; ch++)
+	{
+		for (i = 1; i <= ch-1; i++)
+			_putchar(32);
+		_putchar(92);
+		putchar('\n');
+	}
+	_putchar('\n');
+}
+
