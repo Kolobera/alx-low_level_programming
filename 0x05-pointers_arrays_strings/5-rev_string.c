ff9d992740799e2850fda7c6ccd5f0e2a4fcf373
Kolo <konatealim@gmail.com>
2022-06-13 15:45:11 -0700

    add

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Return: None
+ */
+void rev_string(char *s);
+{
+	int c;
+	int i;
+	char mot[];
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		mot[c] = s[c];
+	}
+	for (i = c - 1; i >= 0; i--)
+	{
+		*s[i] = mot[c - 1 - i]
+	}
+}
