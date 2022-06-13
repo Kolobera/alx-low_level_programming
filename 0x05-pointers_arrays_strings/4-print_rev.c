5a52c150fc8ca394486d25d3784c45b950eb77c6
Kolo <konatealim@gmail.com>
2022-06-13 15:28:53 -0700

    add

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ * Return: None
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = _strlen(s) - 1; c >= 0; c--)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
