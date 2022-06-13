f84f4d5ff22f2105a6d5e7a199c8032646097d0b
Kolo <konatealim@gmail.com>
2022-06-13 15:10:41 -0700

    add

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - gives the len of a str
+ * @s : str
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (i = 0; s[i]!='\0'; i++)
+	{
+	}
+	return (i);
+}
