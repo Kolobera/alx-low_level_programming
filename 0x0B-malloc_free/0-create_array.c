18ba642701d04e16224603e8d8d312ef879e51ca
Kolo <konatealim@gmail.com>
2022-06-27 15:33:56 -0700

    add

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *mot = NULL;
 	unsigned int i;
 
-	mot = malloc(sizeof(int) * size);
+	mot = malloc(sizeof(char) * size);
 	if (size == 0 || mot == NULL)
-		return(NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
-			mot[i] = c;
-		return(mot);
-	}
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		mot[i] = c;
+
+	return (mot);	
 	free(mot);
 }
