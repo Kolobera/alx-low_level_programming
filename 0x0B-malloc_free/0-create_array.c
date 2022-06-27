68f965b8022cec2ef1675944d0d4182e124a7ff8
Kolo <konatealim@gmail.com>
2022-06-27 15:19:05 -0700

    add

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - create an array
+ * @size: size of the array
+ * @c: initialisation of the array
+ * Return: array, 0 or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char mot[] = NULL;
+	int i;
+
+	mot = malloc(sizeof(int), size);
+	if (size == 0 || mot == NULL)
+		return(NULL);
+	else
+	{
+		for (i = 0; i < size; i++)
+			mot[i] = c;
+		return(*mot);
+	}
+	free(mot);
+}
