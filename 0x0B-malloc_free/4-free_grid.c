412730cf7dbe955f04e285b0fa7ec7990ad54e45
Kolo <konatealim@gmail.com>
2022-06-28 04:58:46 -0700

    add

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - free 2-dimensional array
+ * @grid: 2-dimensional array
+ * @height: height of array
+ * Return: None
  */
 
 void free_grid(int **grid, int height)
