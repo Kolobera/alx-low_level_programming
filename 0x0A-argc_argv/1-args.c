a49d780929e9493045ffa0e11379fc1def3dcc9e
Kolo <konatealim@gmail.com>
2022-06-29 12:53:38 -0700

    add

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print numb of args
+ * @argc: int
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
