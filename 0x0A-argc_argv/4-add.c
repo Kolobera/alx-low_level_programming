79920b6060e719b7e9b0080c58b16c5d329bca20
Kolo <konatealim@gmail.com>
2022-06-29 14:47:05 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
 /**
- * main - adding ints
- * @argc: int
- * @argv: array of strings
- * Return: 0
- */
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, j, s = 0;
+	int i, j, add = 0;
 
-	if (argc < 1)
-		printf("0\n");
 	for (i = 1; i < argc; i++)
 	{
 		if (argv[i][0] == '-')
@@ -40,8 +38,8 @@ int main(int argc, char **argv)
 				}
 			}
 		}
-		s += atoi(argv[i]);
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", s);
+	printf("%d\n", add);
 	return (0);
 }
