7e0e66ac8fda0b3fcd8ee53222aac7c5a5ddb69e
Kolo <konatealim@gmail.com>
2022-06-29 12:45:38 -0700

    add

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 
 /**
  * main - print the name of the program
- * @argc - int
- * @argv - array of strings
+ * @argc: int
+ * @argv: array of strings
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
-	int i; 
+	int i;
 
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
