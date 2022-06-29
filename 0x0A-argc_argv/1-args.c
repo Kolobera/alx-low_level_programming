d3e485890075959aec3fd95ef10e1b3b4450bd0f
Kolo <konatealim@gmail.com>
2022-06-29 12:57:09 -0700

    add

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print numb of args
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,6 @@
-int main(int argc; char **argv);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int is_integer(const char *str);
+int get_coin_number(int amount, int nb_coins);
+#endif
