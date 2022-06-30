6c921c98a68c57b7701f389ee36fa9a443b71dcc
Kolo <konatealim@gmail.com>
2022-06-30 05:02:54 -0700

    add

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - alloc memory
+ * @b : int
+ * Return: None
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *n;
+
+
+	n = malloc(b);
+	if ( n == NULL)
+		exit(98);
+	return (n);
+}
