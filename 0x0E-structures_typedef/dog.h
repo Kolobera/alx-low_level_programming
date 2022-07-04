db60cf844f2682af024939e5cb15545b4f128286
Kolo <konatealim@gmail.com>
2022-07-04 04:46:21 -0700

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dogs' owner
+ */
 typedef struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
+#endif
