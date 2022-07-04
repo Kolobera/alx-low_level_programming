68bdc16eda4577c798e69de1ee35da0b08c4bed1
Kolo <konatealim@gmail.com>
2022-07-04 05:38:24 -0700

    add

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print structure dog
+ * @d: struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		print("");
+	else
+	{
+		if (d.name == NULL)
+			printf ("(nil)");
+		else
+		       printf("Name: %s\n", d.name);
+		if (d.age == NULL)
+		       printf("(nil)");
+		else
+			printf("Age: %d\n", d.age);
+		if (d.owner == NULL)
+			printf("(nil)");
+		else
+			printf("Owner: %s\n", d.owner);
+	}
+}
