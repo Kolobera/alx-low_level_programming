149f0f091df69e6a3f5bc9b62c04a1dbd335b20a
Kolo <konatealim@gmail.com>
2022-07-14 15:28:56 -0700

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * add_node - add node at the begining of lk list
+ * @head: node
+ * @str: str
+ * Return: list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	list_t *new;
+
+	new = malloc(size_of(list_t));
+	if (new == NULL)
+	{
+		free(new);
+		printf("Error\n");
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		new->str = 0;
+		new->len = 0;
+		new->head = *head;
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		new->str = stdrup(str);
+		if (new->str == NULL)
+		{
+			free(new);
+			printf("Error\n");
+			return (NULL);
+		}
+		for (i = 0; str[i];)
+			i++;
+		new->len = i;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	return (NULL);
+}
