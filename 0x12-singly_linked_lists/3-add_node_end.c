da91018eef585ae3c4c6bad10e8498bdc642e515
Kolo <konatealim@gmail.com>
2022-07-14 15:55:43 -0700

    add

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add node at the end
+ * @head: lk list
+ * @str: strr
+ * Return: list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i;
+	list_t *new;
+	list_t *endn;
+
+	new = malloc(sizeof(list_t));
+	if (new = NULL)
+	{
+		free(new);
+		printf("Error\n");
+		return (NULL);
+	}
+	if (head = NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		endn = *head;
+		while (endn->next)
+			endn = endn->next;
+		endn->next = new;
+	}
+	if (str == NULL)
+	{
+		newn->str = 0;
+		newn->len = 0;
+		else
+		{
+			new->str = strdup(str);
+			if (new->str == 0)
+			{
+				free(newn);
+				printf("Error\n");
+				return (NULL);
+			}
+			for (i = 0; str[i];)
+				i++;
+			newn->len = i;
+		}
+		new->next = NULL;
+		return (new);
+}
