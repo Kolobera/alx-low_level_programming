ba8d93d301265460be1cbe89b0c34b01879930b7
Kolo <konatealim@gmail.com>
2022-07-25 08:48:40 -0700

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+int str_len(char *);
 /**
  * create_file - create a file
  * @filename: name of file
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_CREAT | O_ WRONLY | O_TRUNC | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
 	if (fd < 0)
 	{
 		if (errno == EEXIST)
@@ -43,6 +43,7 @@ int create_file(const char *filename, char *text_content)
 int str_len(char *s)
 {
 	int len = 0;
+
 	while (s[len++])
 		continue;
 	return (len - 1);
