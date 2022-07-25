e29dce72d480ea49c4bcc8e28066ec6642c9aa41
Kolo <konatealim@gmail.com>
2022-07-25 15:44:16 -0700

    add

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -28,3 +28,15 @@ void get_type(Elf64_Ehdr *ehdr)
 		break;
 	}
 }
+
+/**
+ * main - entry point for program to get header of ELF file
+ * @argc: arg of count
+ * @argv: arg of array
+ * Return: 1 on success, error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
+}
