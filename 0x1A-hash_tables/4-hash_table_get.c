786c0286a0093a3ec7914377f571157850b33006
Kolo <konatealim@gmail.com>
2022-09-22 13:18:03 -0700

    add

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value of a key
+ * @ht: hash table
+ * @key: key to search
+ * Return: value of key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	char *keycopy;
+	hash_node_t *bucket;
+
+	if (!ht || !*key || !key)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	bucket = ht->array[index];
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+			return bucket->value;
+		bucket = bucket->next;
+	}
+	return NULL;
+}
