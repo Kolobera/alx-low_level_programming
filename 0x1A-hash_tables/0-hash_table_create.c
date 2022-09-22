6887fcbad3a1703e44ce030d75ce8b6f63037234
Kolo <konatealim@gmail.com>
2022-09-22 10:38:52 -0700

    add

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
-	new->array = calloc(size, sizeof(hash_table_t));
+	new->array = calloc(size, sizeof(hash_table_t *));
 	if (new->array == NULL)
 	{
 		free(new);
