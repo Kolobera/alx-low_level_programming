f53c5fbfcfa8d9c004deaf6c904686cf70b1fb60
Kolo <konatealim@gmail.com>
2022-09-22 14:14:09 -0700

    add

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table to delete
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *bucket, *to_free;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while(bucket)
+		{
+			to_free = bucket;
+			bucket = bucket->next;
+			if (to_free->key)
+				free(to_free->key);
+			if (to_free->value)
+				free(to_free->value);
+			free(to_free);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
