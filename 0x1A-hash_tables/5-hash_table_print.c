cace4f637f34bfcc0a113c0c3ed503cb36a4fa4e
Kolo <konatealim@gmail.com>
2022-09-22 13:51:47 -0700

    add

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a table
+ * @ht - hash table
+ * Return: None
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *bucket;
+	int surv = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while (bucket)
+		{
+			if (surv)
+				printf(", ");
+			printf("'%s': '%s'", bucket->key, bucket->value);
+			bucket = bucket->next;
+		}
+	}
+	printf("}\n");
+}
