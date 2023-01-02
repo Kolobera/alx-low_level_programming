208fad6efa8bc3d488b74e603d3c1a4fad0127a6
Ousmane Konate <konatealim@gmail.com>
2023-01-02 20:59:24 +0100

    add

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - implements linear search
+ * @array: array to search in
+ * @size: size of array
+ * @value: value to search
+ * Return: position
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+    return (-1);
+}
